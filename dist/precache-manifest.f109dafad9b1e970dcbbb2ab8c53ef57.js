fa41138bff692d6f84fad14b9ca9a24d7af7ba42
Mohamed Hesham <67207376+mohamedhesham221@users.noreply.github.com>
2021-03-22 23:02:53 +0200

    fix

---
+++ dist/precache-manifest.f109dafad9b1e970dcbbb2ab8c53ef57.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd666bd723fd8b02be36",
+    "url": "/css/app.8d40deca.css"
+  },
+  {
+    "revision": "eba6bfce514c5741ef932964df721d76",
+    "url": "/img/abdelbasit.eba6bfce.png"
+  },
+  {
+    "revision": "e0d027d277c31d434127fb6d7d61b056",
+    "url": "/img/ajim.e0d027d2.png"
+  },
+  {
+    "revision": "ebc8b4e0b1b0dad078e1d2edb9fa0468",
+    "url": "/img/cover.ebc8b4e0.jpg"
+  },
+  {
+    "revision": "ce53f620789025c3e9c4856d0b2219e4",
+    "url": "/img/quran.ce53f620.png"
+  },
+  {
+    "revision": "b55cd741b0d38dcadd393f3f6eac549a",
+    "url": "/img/sudis.b55cd741.png"
+  },
+  {
+    "revision": "af4fd70b3b501bc3feed2f5d592f3d51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd666bd723fd8b02be36",
+    "url": "/js/app.f6bf1eb5.js"
+  },
+  {
+    "revision": "82f03cb9fbcaac509502",
+    "url": "/js/chunk-vendors.8426386c.js"
+  },
+  {
+    "revision": "4e25d226e9c0107ebd3ce648bff5d410",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
